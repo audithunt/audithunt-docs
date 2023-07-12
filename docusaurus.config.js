da97d024411a8e60281bf337b2042c31467050ac
Piotr Szacilowski <szacilowski.piotr@gmail.com>
2023-07-13 01:27:19 +0200

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Audit Hunt Documentation',
+  tagline: 'Documentation for Audit Hunt platform',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
